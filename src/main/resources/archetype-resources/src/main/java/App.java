fc0baaa440f9e57351491d2eac8c7c2cba181c83
Sean <sean.-ferguson@hotmail.co.uk>
2026-04-22 21:16:39 +0100

    Phase 1 dependencies and 3 maven project structure scaffold complete.

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.bankportal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
